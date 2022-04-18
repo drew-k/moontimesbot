3bffd1d0d24a4eca04c8b51a100cb78d27631317
drew-k <66277127+drew-k@users.noreply.github.com>
2022-04-18 19:28:31 -0400

    Create bot class and change to Twitter API v2

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 """ Twitter bot that posts the moonrise and moonset times for the day """
 
 import os
+import configparser
 import tweepy
 from dotenv import load_dotenv
 load_dotenv()  # load the environment variables
 
-consumer_key = os.environ.get("CONSUMER_KEY")
-consumer_secret = os.environ.get("CONSUMER_SECRET")
-access_token = os.environ.get("ACCESS_TOKEN")
-access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+class Bot():
+    """ Setup a twitter bot """
+    def __init__(self):
+        self.consumer_key = os.environ.get("CONSUMER_KEY")
+        self.consumer_secret = os.environ.get("CONSUMER_SECRET")
+        self.access_token = os.environ.get("ACCESS_TOKEN")
+        self.access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+        self.client = tweepy.Client(
+            consumer_key=self.consumer_key,
+            consumer_secret=self.consumer_secret,
+            access_token=self.access_token,
+            access_token_secret=self.access_token_secret
+        )
 
-auth = tweepy.OAuth1UserHandler(consumer_key,
-                                consumer_secret,
-                                access_token,
-                                access_token_secret)
+    def send_tweet(self, message: str):
+        """ Send a tweet with the given message """
+        return self.client.create_tweet(text=message)
 
-api = tweepy.API(auth)
+# Initialize bot
+bot = Bot()
 
-for tweet in api.home_timeline():
-    print(tweet.text)
+tweet = bot.send_tweet("Test message from python")
+print(f"https://twitter.com/user/status/{tweet.data['id']}")
+

bfbf559c0a5c61fcef490b794e9d6a3411978192
drew-k <66277127+drew-k@users.noreply.github.com>
2022-04-17 22:17:20 -0400

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+""" Twitter bot that posts the moonrise and moonset times for the day """
+
 import os
+import tweepy
+from dotenv import load_dotenv
+load_dotenv()  # load the environment variables
 
 consumer_key = os.environ.get("CONSUMER_KEY")
 consumer_secret = os.environ.get("CONSUMER_SECRET")
 access_token = os.environ.get("ACCESS_TOKEN")
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuth1UserHandler(consumer_key,
+                                consumer_secret,
+                                access_token,
+                                access_token_secret)
+
+api = tweepy.API(auth)
+
+for tweet in api.home_timeline():
+    print(tweet.text)

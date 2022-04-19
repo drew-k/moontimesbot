3e570d5a3b9db3cf59c491ea583362dd181d3fa6
drew-k <66277127+drew-k@users.noreply.github.com>
2022-04-19 07:52:41 -0400

    Log tweet link to console and check for abnormal moon state

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 """ Twitter bot that posts the moonrise and moonset times for the day """
 
 import os
-import configparser
+from time import strftime
+import requests
 import tweepy
+from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()  # load the environment variables
 
 class Bot():
     """ Setup a twitter bot """
-    def __init__(self):
+    def __init__(self) -> None:
         self.consumer_key = os.environ.get("CONSUMER_KEY")
         self.consumer_secret = os.environ.get("CONSUMER_SECRET")
         self.access_token = os.environ.get("ACCESS_TOKEN")
         self.access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+        self.api_key = os.environ.get("API_KEY")
         self.client = tweepy.Client(
             consumer_key=self.consumer_key,
             consumer_secret=self.consumer_secret,
@@ -20,13 +23,36 @@ class Bot():
             access_token_secret=self.access_token_secret
         )
 
-    def send_tweet(self, message: str):
+    def astronomy_api_call(self) -> requests.Response:
+        return requests.get(f"https://api.ipgeolocation.io/astronomy?apiKey={self.api_key}&location=New%20York,%20US")
+
+    def send_tweet(self, message: str) -> tweepy.Response:
         """ Send a tweet with the given message """
         return self.client.create_tweet(text=message)
 
-# Initialize bot
-bot = Bot()
+def main():
+    # Initialize bot
+    bot = Bot()
 
-tweet = bot.send_tweet("Test message from python")
-print(f"https://twitter.com/user/status/{tweet.data['id']}")
+    # Call the astronomy api
+    api_response = bot.astronomy_api_call().json()  # Get the response in .json format
+    print(api_response["moon_status"])
 
+    # Get the moonrise time
+    unformmated = datetime.strptime(api_response["moonrise"], "%H:%M")  # get datetime object
+    moonrise = unformmated.strftime("%I:%M %p")  # convert to 12-hour
+
+    # Get the moonset time
+    unformmated = datetime.strptime(api_response["moonset"], "%H:%M")  # get datetime object
+    moonset = unformmated.strftime("%I:%M %p")  # convert to 12-hour
+
+    # Send out the tweet
+    if api_response["moon_status"] == "-":
+        tweet = bot.send_tweet(f"The moon will set at {moonset} and rise at {moonrise} today in New York (EST).")  # normal operation
+    else:
+        tweet = bot.send_tweet(f"The moon is {api_response['moon_status'].lower()} today.")  # if the moon is always up or always set within the day
+
+    print(f"Tweet sent out: https://twitter.com/MoonTimesBot/status/{tweet.data['id']}")
+
+if __name__== "__main__":
+    main()
